5d13148b1c2291409c5f25820a430d27b0b3b537
Janah Kuehl <janahs2@hotmail.com>
2024-09-03 19:17:56 -0300

    feat(main): Criando projeto backend

---
+++ backend/saude-facil-back/src/main/java/com/saude_facil_back/SaudeFacilBackApplication.java
@@ -0,0 +1,13 @@
+package com.saude_facil_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeFacilBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeFacilBackApplication.class, args);
+	}
+
+}
---
+++ backend/saude-facil-back/src/test/java/com/saude_facil_back/SaudeFacilBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saude_facil_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeFacilBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
